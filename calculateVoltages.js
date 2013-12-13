62eb958662dc607f850ef6d1efd8e77d05095e9d
forste <stephanfortelny@gmail.com>
2013-12-13 12:50:08 +0100

    improved output

--- calculateVoltages.js
+++ calculateVoltages.js
@@ -9,6 +9,18 @@ var resistors = [
 ];
 var voltage_in = 5;
 var pull_up = 1000;
+var numberOfResistors = 4;
+
+console.log();
+console.log('voltage in: '+voltage_in+' V');
+console.log('pull up: '+pull_up+' ohm');
+var resistors_str = '';
+resistors = resistors.slice(0, numberOfResistors);
+resistors_str = resistors.map(function(r) {
+    return ' '+r+' ohm'
+});
+console.log('resistors: '+resistors_str);
+console.log();
 
 
 function getVoltageOut(numberOfResistors, resistorsTakenOut) {
@@ -25,10 +37,10 @@ function getVoltageOut(numberOfResistors, resistorsTakenOut) {
     return r_2 / (r_1+r_2) * voltage_in;
 }
 
-var numberOfResistors = 4;
 var getVoltageOutForResistors = getVoltageOut.bind(null, numberOfResistors);
-var maxEncoding = Math.pow(2, 4);
+var maxEncoding = Math.pow(2, numberOfResistors);
 
+console.log('encoding  -  voltage out');
 for(var i = 0; i < maxEncoding; i++) {
     var resistorsTakenOut = [];
     var encoding = i; /* binary encoding for resistors, e.g. 1010 ... 4th and 2nd resistor not connected, 3nd and 1st resistor connected */
@@ -67,5 +79,5 @@ for(var i = 0; i < maxEncoding; i++) {
     }
 
     voltage = getVoltageOutForResistors(resistorsTakenOut);
-    console.log(strEncoding+' :  '+voltage+ ' V');
+    console.log(strEncoding+'      -  '+voltage+ ' V');
 }
